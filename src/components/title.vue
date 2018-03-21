aec3010fb7c8b83c32abdc4beac578e9dd9af2af
Sellenite <714627528@qq.com>
2018-03-21 17:00:51 +0800

    init

---
+++ src/components/title.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- 这里的emit一个input事件，是在组件传入v-model时的语法糖，简化代码 -->
+    <div class="title" v-if="value" @click="$emit('input', false)">
+        <p>this is title</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'title',
+    props: {
+        value: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :class="`pages ${$route.name}`">
+        <v-title v-model="isShow"></v-title>
+        <p v-for="(item, index) in list" @click="handleClick(index)" :class="{active: ind === index}">{{item.title}}</p>
+    </div>
+</template>
+
+<script>
+import Title from '@/components/title';
+export default {
+    name: 'index',
+    data() {
+        return {
+            list: [
+                {title: 'AAA'},
+                {title: 'BBB'},
+                {title: 'CCC'}
+            ],
+            isShow: true,
+            ind: 0
+        }
+    },
+    methods: {
+        handleClick(index) {
+            this.ind = index
+        }
+    },
+    components: {
+        'v-title': Title
+    }
+}
+</script>
+
+<style lang="css" scoped>
+    .active {
+        color: red
+    }
+</style>
