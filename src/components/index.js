bb377600326433c8a295b7cb03eda141c85e0c1e
Sellenite <714627528@qq.com>
2019-05-19 14:49:28 +0800

    1

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Vue from 'vue';
 import vPage from './base/vPage';
 import Confirm from './plugins/confirm/index.js';
 
+/* 全局组件 */
 Vue.component('vPage', vPage);
+/* 有了install才能use，具体看官网api */
 Vue.use(Confirm);
